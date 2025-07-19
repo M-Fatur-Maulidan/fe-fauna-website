144a21caa051d8d13e40575eaf3c021fd8a03e24
M-Fatur-Maulidan <mfaturmaulidan.pribadi@gmail.com>
2025-07-19 23:45:33 +0700

    fix : tailwind

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,10 +1,11 @@
 @extends('base')
 
 @section('content')
-    <div class="relative"> <img src="{{ asset('image/fauna/singa_1.jpg') }}" class="w-full" alt="Singa Image">
-        <div class="absolute inset-0 flex flex-column justify-end items-center">
-            <div class="bg-black opacity-50 w-1/2 h-full">
-            </div>
+    {{-- Wadah utama yang menutupi seluruh layar dan menggunakan gambar sebagai background --}}
+    <div class="relative w-screen h-screen bg-cover bg-center" style="background-image: url('{{ asset('image/fauna/singa_1.jpg') }}');">
+        <div class="absolute inset-0 bg-black bg-opacity-50 d-flex flex-col justify-items-end items-center w-full h-full">
+            {{-- Konten di dalam wadah utama, seperti form login --}}
+            <div class="bg-slate-950 opacity-75 w-1/2 h-full"></div>
         </div>
     </div>
 @endsection
